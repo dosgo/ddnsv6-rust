82ddd445ee58acfb177782324ffa231472355b84
dosgo <dosgo@qq.com>
2021-06-28 16:51:42 +0800

    v1.1 use structopt

--- src/main.rs
+++ src/main.rs
@@ -1,101 +1,70 @@
 mod ddns;
-use clap::{App, Arg};
 //use async_std::task;
+use structopt::StructOpt;
+
+#[derive(StructOpt, Debug)]
+#[structopt(name = "basic")]
+struct Opt {
+    // A flag, true if used in the command line. Note doc comment will
+    // be used for the help message of the flag. The name of the
+    // argument will be, by default, based on the name of the field.
+    /// Activate debug mode
+    #[structopt(short, long,default_value = "")]
+    token: String,
+
+    // The number of occurrences of the `v/verbose` flag
+    /// Verbose mode (-v, -vv, -vvv, etc.)
+    #[structopt( long, default_value = "")]
+    domain: String,
+
+    #[structopt(short, long, default_value = "www")]
+    subdomain: String,
+
+    #[structopt(short, long, default_value = "6")]
+    iptype: String,
+
+    #[structopt( long, default_value = "cloudflare")]
+    ddnstype: String,
+
+
+    #[structopt(short, long, default_value = "")]
+    email: String,
+
+    #[structopt(short, long, default_value = "")]
+    apikey: String,
+
+    #[structopt(short, long, default_value = "")]
+    zoneid: String,
+    
+}
 
 fn main() {
-    println!("ddnsv6 v1.0");
-    let matches = App::new("ddnsv6")
-        .version("1.0")
-        .author("dosgo@qq.com")
-        .about("")
-        .arg(
-            Arg::with_name("token")
-                .long("token")
-                .value_name("token")
-                .help("dnspod token"),
-        )
-        .arg(
-            Arg::with_name("domain")
-                .long("domain")
-                .value_name("domain")
-                .help("domain"),
-        )
-        .arg(
-            Arg::with_name("subdomain")
-                .long("subdomain")
-                .value_name("subdomain")
-                .help("sub domain"),
-        )
-        .arg(
-            Arg::with_name("iptype")
-                .long("iptype")
-                .value_name("iptype")
-                .help("ip ver [4,6]"),
-        )
-        .arg(
-            Arg::with_name("ddnstype")
-                .long("ddnstype")
-                .value_name("ddnstype")
-                .help("ddns type dnspod or cloudflare."),
-        )
-        .arg(
-            Arg::with_name("email")
-                .long("email")
-                .value_name("email")
-                .help("cloudflare email."),
-        )
-        .arg(
-            Arg::with_name("apikey")
-                .long("apikey")
-                .value_name("apikey")
-                .help("cloudflare apikey."),
-        )
-        .arg(
-            Arg::with_name("zoneid")
-                .long("zoneid")
-                .value_name("zoneid")
-                .help("cloudflare zoneid."),
-        )
-        .get_matches();
-    let token = matches.value_of("token").unwrap_or("");
-    println!("token : {}", token);
-    let domain = matches.value_of("domain").unwrap_or("");
-    println!("domain : {}", domain);
-    let subdomain = matches.value_of("subdomain").unwrap_or("www");
-    println!("subdomain : {}", subdomain);
-    let iptype = matches.value_of("iptype").unwrap_or("6");
-    println!("iptype : {}", iptype);
-    let ddnstype = matches.value_of("ddnstype").unwrap_or("cloudflare");
-    println!("ddnstype : {}", ddnstype);
-    let email = matches.value_of("email").unwrap_or("");
-    println!("cloudflare email : {}", email);
-    let apikey = matches.value_of("apikey").unwrap_or("");
-    println!("cloudflare apikey : {}", apikey);
-    let zoneid = matches.value_of("zoneid").unwrap_or("");
-    println!("cloudflare zoneid : {}", zoneid);
+    let opt = Opt::from_args();
+    println!("ddnsv6 v1.1");
+    println!("param:{:#?}", opt);
 
-    if domain == "" {
+    if opt.domain == "" {
         println!("domain Not allowed to be empty!");
         return;
     }
 
-    if ddnstype == "cloudflare" && (email == "" || apikey == "" || zoneid == "") {
+    if opt.ddnstype == "cloudflare" && (opt.email == "" || opt.apikey == "" || opt.zoneid == "") {
         println!("Using cloudflare requires email apikey zoneid parameter ");
         return;
     }
-    if ddnstype == "ddns" && token == "" {
+    if opt.ddnstype == "ddns" && opt.token == "" {
         println!("Using cloudflare requires token parameter ");
         return;
     }
 
     ddns::start_ddns_check(
-        ddnstype.to_string(),
-        iptype.to_string(),
-        domain.to_string(),
-        subdomain.to_string(),
-        token.to_string(),
-        email.to_string(),
-        apikey.to_string(),
-        zoneid.to_string(),
+        opt.ddnstype.to_string(),
+        opt.iptype.to_string(),
+        opt.domain.to_string(),
+        opt.subdomain.to_string(),
+        opt.token.to_string(),
+        opt.email.to_string(),
+        opt.apikey.to_string(),
+        opt.zoneid.to_string(),
     );
 }
